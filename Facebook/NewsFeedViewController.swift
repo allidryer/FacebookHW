0ec7652db48b454b17e4f27e6894d44b95c04d53
Alli Dryer <awdryer@gmail.com>
2014-10-05 20:46:11 -0700

    Transition working okay in both directions

--- Facebook/NewsFeedViewController.swift
+++ Facebook/NewsFeedViewController.swift
@@ -8,12 +8,18 @@
 
 import UIKit
 
-class NewsFeedViewController: UIViewController {
+class NewsFeedViewController: UIViewController, UIViewControllerTransitioningDelegate, UIViewControllerAnimatedTransitioning {
 
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var feedImageView: UIImageView!
+    @IBOutlet weak var wedding1ImageView: UIImageView!
+    @IBOutlet weak var wedding2ImageView: UIImageView!
+    @IBOutlet weak var wedding3ImageView: UIImageView!
+    @IBOutlet weak var wedding4ImageView: UIImageView!
+    @IBOutlet weak var wedding5ImageView: UIImageView!
     
     var imageViewToSegue: UIImageView!
+    var isPresenting: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,11 +33,80 @@ class NewsFeedViewController: UIViewController {
         performSegueWithIdentifier("feedToPhotoViewer", sender: self)
     }
     
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+    override func prepareForSegue(segue: (UIStoryboardSegue!), sender: AnyObject!) {
         var destinationViewController = segue.destinationViewController as PhotoViewController
-        
         destinationViewController.image = self.imageViewToSegue.image
+        destinationViewController.modalPresentationStyle = UIModalPresentationStyle.Custom
+        destinationViewController.transitioningDelegate = self
+    }
+    
+    func animationControllerForPresentedController(presented: UIViewController!, presentingController presenting: UIViewController!, sourceController source: UIViewController!) -> UIViewControllerAnimatedTransitioning! {
+        isPresenting = true
+        return self
+    }
+    
+    func animationControllerForDismissedController(dismissed: UIViewController!) -> UIViewControllerAnimatedTransitioning! {
+        isPresenting = false
+        return self
+    }
+    
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
+        // The value here should be the duration of the animations scheduled in the animationTransition method
+        return 0.4
+    }
+    
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        println("animating transition")
+        var containerView = transitionContext.containerView()
+        var toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
+        var fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
         
+        var window = UIApplication.sharedApplication().keyWindow
+        var frame = window.convertRect(imageViewToSegue.frame, fromView: scrollView)
+        var copyImageViewToSegue = UIImageView(frame: frame)
+        var scalefactor = window.frame.width / copyImageViewToSegue.frame.width
+        
+        var photoViewImageCenter = CGPoint(x: 320/2, y: 287)
+        var imageViewCenterX = self.imageViewToSegue.center.x
+        var imageViewCenterY = self.imageViewToSegue.center.y + 110
+
+        if (isPresenting) {
+            copyImageViewToSegue.image = imageViewToSegue.image
+            window.addSubview(copyImageViewToSegue)
+            containerView.addSubview(toViewController.view)
+            toViewController.view.alpha = 0
+            copyImageViewToSegue.contentMode = UIViewContentMode.ScaleAspectFill
+            copyImageViewToSegue.clipsToBounds = true
+            
+            UIView.animateWithDuration(0.4, animations: { () -> Void in
+                copyImageViewToSegue.transform = CGAffineTransformMakeScale(scalefactor, scalefactor)
+                copyImageViewToSegue.center.x = 320 / 2
+                copyImageViewToSegue.center.y = 287
+                toViewController.view.alpha = 1
+                }, completion: { (finished: Bool) -> Void in
+                    transitionContext.completeTransition(true)
+                    copyImageViewToSegue.removeFromSuperview()
+            })
+        } else {
+            copyImageViewToSegue.image = imageViewToSegue.image
+            window.addSubview(copyImageViewToSegue)
+            containerView.addSubview(fromViewController.view)
+            copyImageViewToSegue.contentMode = UIViewContentMode.ScaleAspectFill
+            copyImageViewToSegue.clipsToBounds = true
+            copyImageViewToSegue.center = photoViewImageCenter
+            
+            copyImageViewToSegue.transform = CGAffineTransformMakeScale(scalefactor, scalefactor)
+            fromViewController.view.alpha = 1
+            UIView.animateWithDuration(0.4, animations: { () -> Void in
+                copyImageViewToSegue.transform = CGAffineTransformMakeScale(1, 1)
+                copyImageViewToSegue.center.x = imageViewCenterX
+                copyImageViewToSegue.center.y = imageViewCenterY
+                fromViewController.view.alpha = 0
+                }) { (finished: Bool) -> Void in
+                    transitionContext.completeTransition(true)
+                    copyImageViewToSegue.removeFromSuperview()
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
--- Facebook/PhotoViewController.swift
+++ Facebook/PhotoViewController.swift
@@ -8,12 +8,26 @@
 
 import UIKit
 
-class PhotoViewController: UIViewController {
+class PhotoViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var photoActionsImageView: UIImageView!
+    @IBOutlet weak var doneButton: UIButton!
+    
     var image: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imageView.image = image
 
+        var scrollWidth: CGFloat  = self.view.frame.size.width
+        var scrollHeight: CGFloat  = self.view.frame.size.height
+        self.scrollView!.contentSize = CGSizeMake(scrollWidth, scrollHeight + 100)
+        
+        scrollView.delegate = self
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,6 +35,45 @@ class PhotoViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func onDoneButtonPress(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView!) {
+        UIView.animateWithDuration(0.5, animations: { () -> Void in
+            self.scrollView.backgroundColor = UIColor(white: 0, alpha: 0)
+            self.photoActionsImageView.alpha = 0
+            self.doneButton.alpha = 0
+        }, completion: nil)
+        println("scrolling!")
+    }
+    
+    func scrollViewWillBeginDragging(scrollView: UIScrollView!) {
+    
+    }
+    
+    func scrollViewDidEndDragging(scrollView: UIScrollView!,
+        willDecelerate decelerate: Bool) {
+            //println("Content Offset: \(scrollView.contentOffset.y)")
+            if scrollView.contentOffset.y <= -100 {
+                //println("DISMISSED")
+                dismissViewControllerAnimated(true, completion: nil)
+            } else {
+                UIView.animateWithDuration(0.5, animations: { () -> Void in
+                    self.scrollView.backgroundColor = UIColor(white: 0, alpha: 1)
+                    self.photoActionsImageView.alpha = 1
+                    self.doneButton.alpha = 1
+                    }, completion: nil)
+                
+            }
+            
+    }
+    
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView!) {
+        // This method is called when the scrollview finally stops scrolling.
+    }
+
     
 
     /*
